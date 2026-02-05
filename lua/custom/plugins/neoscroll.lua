4c5f61034b548272b409c5d2c3f3ab46564031ee
Gokul <gokulgk9402@gmail.com>
2026-02-05 17:00:08 +0530

    Add neoscroll, smear-cursor

---
+++ lua/custom/plugins/neoscroll.lua
@@ -0,0 +1,9 @@
+return {
+  'karb94/neoscroll.nvim',
+  opts = {
+    -- Use a more natural easing function
+    easing = 'cubic',
+    -- Disable smooth scrolling for searches to keep them snappy
+    stop_when_search = true,
+  },
+}
