acfe0065666ee7ad8806dcb9214a61c1c48302e0
Gokul Kannan <gokulscodes@gmail.com>
2026-02-07 13:39:39 +0530

    show windsurf status in lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -10,9 +10,26 @@ return {
     sections = {
       lualine_x = {
         {
-          function() return '🌊 ' .. vim.fn['codeium#GetStatusString']() end,
+          function()
+            local status = require('codeium.virtual_text').status()
+
+            if status.state == 'idle' then
+              -- Output was cleared, for example when leaving insert mode
+              return '🌊' .. 'Idle'
+            end
+
+            if status.state == 'waiting' then
+              -- Waiting for response
+              return '🌊' .. 'Waiting...'
+            end
+
+            if status.state == 'completions' and status.total > 0 then return '🌊' .. string.format('%d/%d', status.current, status.total) end
+
+            return '🌊' .. ' 0 '
+          end,
           color = { fg = '#61afef' },
         },
+
         'encoding',
         'fileformat',
         'filetype',
@@ -20,3 +37,4 @@ return {
     },
   },
 }
+
